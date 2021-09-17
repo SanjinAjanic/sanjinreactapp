72eb577718df522c9d3f2cc41e79f80283d36bee
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-09-17 14:24:50 +0200

    lagt in HomeViews, SingInViews

---
+++ src/Views/homeview/HomeView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Homeview = () => {
+    return (
+        <div>
+            <h1>You are in the homeview</h1>
+        </div>
+    );
+};
---
+++ src/Views/singinview/SignInView.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SignInView = () => {
+    return (
+        <div>
+            <h1>You are in the SignInView</h1>
+        </div>
+    )
+}
