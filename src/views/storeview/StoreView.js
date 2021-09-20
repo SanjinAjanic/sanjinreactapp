6c3f4d1a789f5df90368139d1df5688e161dd5be
SanjinAjanic <74593957+SanjinAjanic@users.noreply.github.com>
2021-09-20 18:59:57 +0200

    lagt in useState och nya knappar med +- i min storeViiew

--- src/views/storeview/StoreView.js
+++ src/views/storeview/StoreView.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const StoreView = () => {
+    const [items, setItems] = useState(0);
     return (
         <div>
-            <h1>This is the Storeview</h1>
+            <h1>Our Products</h1>
+            <buttom onClick={() => setItems(items + 1)}>+</buttom>
+            <span>{items}</span>
+            <buttom onClick={() => {
+                if(items >= 1) setItems(items - 1)}}>-</buttom>
         </div>
     );
 };
